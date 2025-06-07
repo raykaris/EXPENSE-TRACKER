630235a5dc1568127bdb51a1cd68c752f3f6ac16
raykaris <ryanmuthoni050@gmail.com>
2025-06-07 14:18:13 -0400

    Frontend update

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,10 +1,14 @@
-import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { Slot } from 'expo-router';
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import SafeScreen from '../components/SafeScreen';
 
 export default function RootLayout() {
   return (
-    <SafeScreen>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeScreen>
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
   );
 }
